ebea9c31cdbf53f0cde96d5efded958ae59b4e6d
Orpheric <innocent.azainon@gmail.com>
2020-08-02 00:01:36 +0100

    First Commit

---
+++ src/test/java/com/orpheric/ExpenseRestApi/ExpenseRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orpheric.ExpenseRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
