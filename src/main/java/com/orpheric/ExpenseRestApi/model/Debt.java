3ab54fd17785ade0d46c3bb8fae3e14f5848f4fb
Orpheric <innocent.azainon@gmail.com>
2020-08-02 00:12:38 +0100

    Ajout de configuaration et d'attributs pour la classe debts

--- src/main/java/com/orpheric/ExpenseRestApi/model/Debt.java
+++ src/main/java/com/orpheric/ExpenseRestApi/model/Debt.java
@@ -12,6 +12,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 @Entity
 public class Debt {
 
@@ -27,15 +29,22 @@ public class Debt {
 	@ManyToOne(cascade = CascadeType.PERSIST,fetch = FetchType.LAZY)
 	@JoinColumn(referencedColumnName="id",name="user_id")
 	private User user;
+	private String status;
+	private LocalDate dateDue;
 	public Debt() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Debt(String title, String creditor, LocalDate date) {
+
+
+	public Debt(String title, String creditor, LocalDate date, User user, String status, LocalDate dateDue) {
 		super();
 		this.title = title;
 		this.creditor = creditor;
 		this.date = date;
+		this.user = user;
+		this.status = status;
+		this.dateDue = dateDue;
 	}
 
 
@@ -78,6 +87,30 @@ public class Debt {
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+
+	public LocalDate getDateDue() {
+		return dateDue;
+	}
+
+
+
+	public void setDateDue(LocalDate dateDue) {
+		this.dateDue = dateDue;
 	}
 
 	
